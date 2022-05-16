283334bd86948942e730082e40c97dda0392f4c9
Valerio Gherardi <valerio.gherardi@voicemod.net>
2022-05-16 21:21:07 +0200

    Fix DDL of arxiv table

--- etl_runners/etl/arxiv_etl.py
+++ etl_runners/etl/arxiv_etl.py
@@ -7,22 +7,23 @@ from feedparser import parse as parse_feed
 import pandas as pd
 
 from .etl import ETL
+from sqlalchemy import inspect
 
 ARXIV_API_ENDPOINT = "http://export.arxiv.org/api/query"
-ARXIV_FEED_COLUMNS = [
-    'id'
-    , 'published'
-    , 'updated'
-    , 'authors'
-    , 'author'
-    , 'title'
-    , 'summary'
-    , 'link'
-    , 'arxiv_doi'
-    , 'arxiv_comment'
-    , 'arxiv_journal_ref'
-    , 'tags'
-    ]
+ARXIV_FEED_COLUMNS = {
+    'id': 'id'
+    , 'published': 'published'
+    , 'updated': 'updated'
+    , 'authors': 'authors'
+    , 'author': 'submitter'
+    , 'title': 'title'
+    , 'summary': 'summary'
+    , 'link': 'link'
+    , 'arxiv_doi': 'arxiv_doi'
+    , 'arxiv_comment': 'arxiv_comment'
+    , 'arxiv_journal_ref': 'arxiv_journal_ref'
+    , 'tags': 'tags'
+    }
 
 class ArxivETL(ETL):
     def __init__(self, start_date, end_date, **kwargs):
@@ -47,11 +48,11 @@ class ArxivETL(ETL):
 
     def transform(self):
         # TODO: append columns with NULLs if the arxiv feed does not return all variables
-        for col_name in ARXIV_FEED_COLUMNS:
+        for col_name in ARXIV_FEED_COLUMNS.keys():
             if col_name not in self.data:
                 self.data[col_name] = None
-        self.data = self.data[ARXIV_FEED_COLUMNS]
-        self.data = self.data.rename(columns = {'author': 'submitter'})
+        self.data = self.data[ARXIV_FEED_COLUMNS.keys()]
+        self.data = self.data.rename(columns = ARXIV_FEED_COLUMNS)
         self.data['tags'] = [
             '|'.join( [tag['term'] for tag in tag_list] )
             for tag_list in self.data['tags']
@@ -72,7 +73,16 @@ class ArxivETL(ETL):
         )
 
         # TODO: either the above solution, or create a wrapper for this upsert query!
-        on_conflict_updates = ", ".join([col_name + " = EXCLUDED." + col_name for col_name in ARXIV_FEED_COLUMNS ])
+        if not inspect(self._eng).has_table("arxiv", "raw"):
+            self._eng.execute(
+                "CREATE TABLE raw.arxiv AS ( SELECT * FROM raw.arxiv_dump ) WITH NO DATA"
+            )
+            self._eng.execute("ALTER TABLE raw.arxiv ADD PRIMARY KEY (id)")
+
+        on_conflict_updates = ", ".join([col_name + " = EXCLUDED." + col_name for col_name in ARXIV_FEED_COLUMNS.values() ])
+
+
+
         self._eng.execute(
             """
             INSERT INTO raw.arxiv
